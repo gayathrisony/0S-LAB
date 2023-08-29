a18d1d0a978d108511f6e648eb8c2fbaae66f98e
gayathrisony <141231381+gayathrisony@users.noreply.github.com>
2023-08-29 12:55:50 +0530

    Update fcfs.c

--- fcfs.c
+++ fcfs.c
@@ -11,8 +11,7 @@ scanf("%d", &bt[i]);
 }
 wt[0] = wtavg = 0;
 tat[0] = tatavg = bt[0];
-for(i=1;i<n;i++)
-{
+for(i=1;i<n;i++){
 wt[i] = wt[i-1] +bt[i-1];
 tat[i] = tat[i-1] +bt[i];
 wtavg = wtavg + wt[i];
